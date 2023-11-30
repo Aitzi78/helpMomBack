5139db6686a997d86eac7cd038918b13f4b40b83
Aitziber Ruiz <aitziberruiz@Aitzibers-Mac-mini.local>
2023-11-30 18:03:30 +0100

    heroku ready

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-import os
+#import os
 
-from os.path import abspath, dirname
+#from os.path import abspath, dirname
 
-BASE_DIR = dirname(abspath(__file__))
+#BASE_DIR = dirname(abspath(__file__))
 
 
 class ApplicationConfig:
@@ -10,6 +10,7 @@ class ApplicationConfig:
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(
-        BASE_DIR, "database", "helpMom.db"
-    )
+    SQLALCHEMY_DATABASE_URI = "postgresql://ebnfacoqhqzgwt:4f18463556d743640aa0432d1bb440feb54b77e0c507ebcb57707412e2e890ee@ec2-54-78-142-10.eu-west-1.compute.amazonaws.com:5432/deui5mo5acfoso"
+    #SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(
+     #   BASE_DIR, "database", "helpMom.db"
+    #)
